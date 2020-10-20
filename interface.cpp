0927a2db8e4eada710b83ce3bb3d958edf7a88e8
Ivan Yeremenko <ivanyeremenko3@gmail.com>
2020-10-21 01:33:33 +0300

    changed structure

--- interface.cpp
+++ interface.cpp
@@ -4,25 +4,23 @@
 
 typedef std::vector<std::vector<int>> Smegn;
 
-std::vector<std::string> ReadInput(int& matrSize, int& n, int& pathLength, std::istream& inputStream = std::cin);
+std::vector<std::string> ReadInput(int& numberVertices, int& numberPaths, int& pathLength, std::istream& inputStream = std::cin);
 
-Smegn ConvertToSmegn(const std::vector<std::string>& input, int matrSize);
+Smegn ConvertToSmegn(const std::vector<std::string>& paths, int numberVertices);
 
-Smegn MultiplyMatrices(const Smegn& a, const Smegn& b, int mod);
+Smegn MultiplyMatrices(const Smegn& firstMatrix, const Smegn& secondMatrix, int mod);
 
-Smegn BinpowMatrices(const Smegn& a, int n, int mod);
+Smegn BinpowMatrices(const Smegn& matrix, int power, int mod);
 
-int64_t CountNumberPaths(const Smegn& matrix, int startVertex, int mod);
+int CountNumberPaths(const std::vector<std::string>& paths, int pathLength, int numberVertices, int startVertex, int mod);
 
 void WriteResult(int result, std::ostream& outputStream = std::cout);
 
 int main() {
-    int matrSize, n, pathLength;
-    std::vector<std::string> paths = ReadInput(matrSize, n, pathLength);
-    Smegn matrix = ConvertToSmegn(paths, matrSize);
+    int numberVertices, numberPaths, pathLength;
+    std::vector<std::string> paths = ReadInput(numberVertices, numberPaths, pathLength);
     const int mod = 1000000007;
-    matrix = BinpowMatrices(matrix, pathLength, mod);
-    int result = CountNumberPaths(matrix, 0, mod);
+    int result = CountNumberPaths(paths, pathLength, numberVertices, 0, mod);
     WriteResult(result);
     return 0;
 }

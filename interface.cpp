64a63292483b4144511aa000a2a7fd30b877d86c
Ivan Yeremenko <ivanyeremenko3@gmail.com>
2020-10-21 11:39:57 +0300

    add struct

--- interface.cpp
+++ interface.cpp
@@ -4,7 +4,14 @@
 
 typedef std::vector<std::vector<int>> Smegn;
 
-std::vector<std::string> ReadInput(int& numberVertices, int& numberPaths, int& pathLength, std::istream& inputStream = std::cin);
+struct InputData {
+    int numberVertices;
+    int numberPaths;
+    int pathLength;
+    std::vector<std::string> paths;
+};
+
+InputData ReadInput(std::istream& inputStream = std::cin);
 
 Smegn ConvertToSmegn(const std::vector<std::string>& paths, int numberVertices);
 
@@ -12,15 +19,14 @@ Smegn MultiplyMatrices(const Smegn& firstMatrix, const Smegn& secondMatrix, int 
 
 Smegn BinpowMatrices(const Smegn& matrix, int power, int mod);
 
-int CountNumberPaths(const std::vector<std::string>& paths, int pathLength, int numberVertices, int startVertex, int mod);
+int64_t CountNumberPaths(const std::vector<std::string>& paths, int pathLength, int numberVertices, int startVertex, int mod);
 
-void WriteResult(int result, std::ostream& outputStream = std::cout);
+void WriteResult(int64_t result, std::ostream& outputStream = std::cout);
 
 int main() {
-    int numberVertices, numberPaths, pathLength;
-    std::vector<std::string> paths = ReadInput(numberVertices, numberPaths, pathLength);
+    InputData inputData = ReadInput();
     const int mod = 1000000007;
-    int result = CountNumberPaths(paths, pathLength, numberVertices, 0, mod);
+    int64_t result = CountNumberPaths(inputData.paths, inputData.pathLength, inputData.numberVertices, 0, mod);
     WriteResult(result);
     return 0;
 }

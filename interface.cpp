d293d37afb4d895a1846d2f57e4add74d7f73e4f
Ivan Yeremenko <ivanyeremenko3@gmail.com>
2020-10-22 22:20:51 +0300

    changed names

--- interface.cpp
+++ interface.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-#include<string>
-#include<vector>
+#include <iostream>
+#include <string>
+#include <vector>
 
 typedef std::vector<std::vector<int>> Matrix;
+
+const int MODULUS = 1000000007;
 
 struct Edge {
     int outgoingVertex;
     int incomingVertex;
 };
 
-struct InputData {
+struct GraphParameters {
     int numberVertices;
     int pathLength;
     std::vector<Edge> edges;
 };
 
-InputData ReadInput(std::istream& inputStream = std::cin);
+GraphParameters ReadInput(std::istream& inputStream = std::cin);
 
-Matrix ConvertEdgesToAdjacencyMatrix(const std::vector<Edge>& edges, int numberVertices);
-
-const int module = 1000000007;
+Matrix ConvertEdgesToAdjacencyMatrix(const std::vector<Edge>& edges, const int numberVertices);
 
 Matrix MultiplyMatrices(const Matrix& firstMatrix, const Matrix& secondMatrix);
 
-Matrix RaiseMatrixToPower(const Matrix& matrix, int power);
+Matrix RaiseMatrixToPower(const Matrix& matrix, const int power);
 
-int CountNumberPaths(const InputData& inputData, int startVertex);
+int CountNumberPaths(const GraphParameters& graphParameters, const int startVertex);
 
-void WriteResult(int result, std::ostream& outputStream = std::cout);
+void WriteResult(const int result, std::ostream& outputStream = std::cout);
 
 int main() {
-    const InputData inputData = ReadInput();
-    const int result = CountNumberPaths(inputData, 0);
-    WriteResult(result);
+    const GraphParameters graphParameters = ReadInput();
+    const int numberPathsFromFirstVertex = CountNumberPaths(graphParameters, 0);
+    WriteResult(numberPathsFromFirstVertex);
     return 0;
 }
